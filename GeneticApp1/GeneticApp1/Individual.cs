94056979384934e7e20d6df5f7cae1479d95005c
Neil <n.sidlow@leedsbeckett.ac.uk>
2019-03-20 14:06:51 +0000

    initial convert

---
+++ GeneticApp1/GeneticApp1/Individual.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneticApp1
+{
+    public class Individual
+    {
+        public int fitness = 0;
+        public List<int> genes = new List<int>();
+        public int geneLength = 5;
+
+        public Individual()
+        {
+            Random rn = new Random();
+
+            //Set genes randomly for each individual
+            for (int i = 0; i < genes.Count; i++)
+            {
+                genes[i] = Math.Abs(rn.Next() % 2);
+            }
+
+            fitness = 0;
+        }
+
+        //Calculate fitness
+        public void calcFitness()
+        {
+
+            fitness = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (genes[i] == 1)
+                {
+                    ++fitness;
+                }
+            }
+        }
+    }
+}
---
+++ GeneticApp1/GeneticApp1/Population.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneticApp1
+{
+    public class Population
+    {
+
+        int popSize = 10;
+        public Individual[] individuals;
+        public int fittest = 0;
+
+        //Initialize population
+        public void initializePopulation(int size)
+        {
+            for (int i = 0; i < individuals.Count; i++)
+            {
+                individuals[i] = new Individual();
+            }
+        }
+
+        //Get the fittest individual
+        public Individual getFittest()
+        {
+            int maxFit = int.MinValue;
+            int maxFitIndex = 0;
+            for (int i = 0; i < individuals.Count; i++)
+            {
+                if (maxFit <= individuals[i].fitness)
+                {
+                    maxFit = individuals[i].fitness;
+                    maxFitIndex = i;
+                }
+            }
+            fittest = individuals[maxFitIndex].fitness;
+            return individuals[maxFitIndex];
+        }
+
+        //Get the second most fittest individual
+        public Individual getSecondFittest()
+        {
+            int maxFit1 = 0;
+            int maxFit2 = 0;
+            for (int i = 0; i < individuals.Count; i++)
+            {
+                if (individuals[i].fitness > individuals[maxFit1].fitness)
+                {
+                    maxFit2 = maxFit1;
+                    maxFit1 = i;
+                }
+                else if (individuals[i].fitness > individuals[maxFit2].fitness)
+                {
+                    maxFit2 = i;
+                }
+            }
+            return individuals[maxFit2];
+        }
+
+        //Get index of least fittest individual
+        public int getLeastFittestIndex()
+        {
+            int minFitVal = int.MaxValue;
+            int minFitIndex = 0;
+            for (int i = 0; i < individuals.Count; i++)
+            {
+                if (minFitVal >= individuals[i].fitness)
+                {
+                    minFitVal = individuals[i].fitness;
+                    minFitIndex = i;
+                }
+            }
+            return minFitIndex;
+        }
+
+        //Calculate fitness of each individual
+        public void calculateFitness()
+        {
+
+            for (int i = 0; i < individuals.Count; i++)
+            {
+                individuals[i].calcFitness();
+            }
+            getFittest();
+        }
+    }
+}
